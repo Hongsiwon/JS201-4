a044f9ec31697e7ec46159da33499fd86fdd8921
Hongsiwon <hsw010730@naver.com>
2021-04-20 11:01:11 +0900

    가나다

---
+++ ch5.js
@@ -0,0 +1,19 @@
+//let foo = function(){
+//    console.log("첫줄");
+//    console.log("둘째줄");
+//}
+
+//let ar = 135;
+
+//foo();
+//console.log(foo);
+//console.log(bar);
+
+//선언적 함수
+function foo(){
+    console.log("첫줄");
+    console.log("둘째줄");
+}
+
+foo()
+console.log(foo);
---
+++ hello.js
@@ -0,0 +1,3 @@
+const con1 = "문자열 상수";
+con1 = ""
+console.log(con1);
